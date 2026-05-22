5e7a35c8e97557506c617a5c5f06cc35997b2204
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2026-05-22 16:46:32 -0300

    ADD: G01E01 Persona.java TODO: lanzar excepción en setters de nombre y apellido

---
+++ src/g01/e01/Persona.java
@@ -0,0 +1,81 @@
+package g01.e01;
+
+import java.time.Year;
+
+/**
+ *
+ * @author Alberto Martín Capurro
+ */
+public class Persona {
+    private String  nombre;
+    private String  apellido;
+    private int     anioDeNacimiento;
+    
+    private static final int EDAD_MAXIMA = 140;
+    
+    Persona(String nombre, String apellido, int anioDeNacimiento)
+    {
+        setNombre(nombre);
+        setApellido(apellido);
+        setAnioDeNacimiento(anioDeNacimiento);
+    }
+    
+    public void setNombre(String nombre)
+    {
+        if(nombre!=null && nombre.length()>=3)
+        {
+            this.nombre = nombre;
+        }
+    }
+    
+    public void setApellido(String apellido)
+    {
+        if(apellido!=null && apellido.length()>=3)
+        {
+            this.apellido = apellido;
+        }
+    }
+    
+    private boolean esAnioFuturo(int anio)
+    {
+        return anio > Year.now().getValue();
+    }
+    
+    private boolean esAnioDemasiadoAntiguo(int anio)
+    {
+        return anio < Year.now().getValue() - EDAD_MAXIMA;
+    }
+    
+    public void setAnioDeNacimiento(int anioDeNacimiento) {
+        if(esAnioFuturo(anioDeNacimiento) || esAnioDemasiadoAntiguo(anioDeNacimiento))
+        {
+            throw new IllegalArgumentException("Anio de nacimiento invalido: " + anioDeNacimiento);
+        }
+        this.anioDeNacimiento = anioDeNacimiento;
+    }
+    
+    public String getNombreCompleto()
+    {
+        return nombre + " " + apellido;
+    }
+    
+    public int getEdadActual()
+    {
+        return Year.now().getValue() - anioDeNacimiento;
+    }
+    
+    public boolean esMayorQue(int edad) {
+        return (getEdadActual() > edad);
+    }
+
+    
+    void setNombreYApellido(String nuevoNombre, String nuevoApellido) {
+        setNombre(nuevoNombre);
+        setApellido(nuevoApellido);
+    }
+    
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", apellido=" + apellido + ", anioDeNacimiento=" + anioDeNacimiento + '}';
+    }
+}

f2eeead038f8b39d6cd357505c7fcdbff5bfccc4
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2026-05-24 15:10:14 -0300

    ADD: G01E03 Principal.java

---
+++ src/g01/e03/Principal.java
@@ -0,0 +1,41 @@
+/*
+    3) Modelá la clase CuentaBancaria, la cual posea como atributos la clave
+    bancaria uniforme (CBU), el tipo (caja de ahorro o cuenta corriente) y el 
+    saldo (inicialmente en 0). Luego, implementá métodos que permitan:
+     Obtener el saldo actual.
+     Depositar dinero en la cuenta (actualizando el saldo).
+     Extraer dinero de la cuenta (actualizando el saldo). Solo puede quedar
+    en saldo negativo si es cuenta corriente.
+     Obtener los últimos 3 dígitos del CBU.
+*/
+package g01.e03;
+
+/**
+ *
+ * @author Alberto Martín Capurro
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CuentaBancaria cuenta[] = {
+            new CuentaBancaria(TipoDeCuenta.CAJA_DE_AHORRO),
+            new CuentaBancaria(TipoDeCuenta.CUENTA_CORRIENTE)
+        };
+        
+        cuenta[0].depositar(500);
+        //cuenta[0].extraer(800); //throws exception OK
+        cuenta[1].depositar(500);
+        cuenta[1].extraer(800);
+                
+        for (CuentaBancaria cuentaBancaria : cuenta) {
+            System.out.println("Ultimos 3 digitos: " + cuentaBancaria.obtenerUltimos3DigitosDeCBU());
+            System.out.printf("Saldo actual: $%12.2f\n",cuentaBancaria.obtenerSaldoActual());
+            System.out.println(cuentaBancaria);
+            System.out.println("----");
+        }
+    }
+
+}

2a037c77806bbb9d43644fbb8b13f5bc1e520501
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2026-05-30 18:53:34 -0300

    DOC: G01E05 Principal.java, añade enunciado G01E06

--- src/g01/e05/Principal.java
+++ src/g01/e05/Principal.java
@@ -16,6 +16,11 @@
         denominador.
     Fracciones impropias: son aquellas en las que el numerador es mayor que el
         denominador, pero no múltiplo.
+
+    6) Refactoreá las clases de los cuatros ejercicios anteriores con los siguientes cambios:
+     Agregar un constructor que inicialice todos sus atributos por parámetro.
+     Establecer sus atributos como privados y colocar los getters/setters que correspondan.
+     Implementar el método toString() y comprobar su funcionamiento
 */
 package g01.e05;
 

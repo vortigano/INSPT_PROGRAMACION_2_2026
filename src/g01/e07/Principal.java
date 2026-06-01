b81e64acc106799f3eb40ea87f7495d633ddd5e0
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2026-05-31 23:01:59 -0300

    DOC: G01E07 Principal.java, añade enunciado

--- src/g01/e07/Principal.java
+++ src/g01/e07/Principal.java
@@ -1,3 +1,22 @@
+/*
+    7) Se precisa un robot (que tiene nombre) que permita atender llamadas 
+    telefónicas. La compañía puede detectar algunos clientes según su número de 
+    teléfono, sin embargo, en otros casos no. Por ello, el robot debe ser capaz 
+    de procesar alguno de los siguientes métodos homónimos:
+
+     saludar(): void
+     Emite por consola un saludo diciendo: 
+     "Hola, mi nombre es ___. ¿En qué puedo ayudarte?".2
+
+     saludar(Persona): void
+     Emite por consola un saludo diciendo:
+     "Hola ___, mi nombre es ___. ¿En qué puedo ayudarte?".3
+
+     2 El "___" debe reemplazarse por el nombre del robot.
+     3 El primer "___" debe reemplazarse por el nombre completo de la persona y
+       el segundo "___" debe reemplazarse por el nombre del robot.
+     4 No olvides instanciar un objeto Robot en el main
+*/
 package g01.e07;
 
 /**

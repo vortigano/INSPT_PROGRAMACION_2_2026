337475ea1906e1375850d5f120c9b09d1d6fdae9
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2026-06-15 15:31:31 -0300

    ADD: G01E14 Textura.java

---
+++ src/g01/e14/Textura.java
@@ -0,0 +1,13 @@
+package g01.e14;
+
+/**
+* Código generado por la app UXFtoJava by Charly Cimino
+* @see https://github.com/CharlyCimino/uxf-to-java
+*/
+public enum Textura {
+
+    ESPUMOSA,
+    NORMAL,
+    LIQUIDA;
+
+}

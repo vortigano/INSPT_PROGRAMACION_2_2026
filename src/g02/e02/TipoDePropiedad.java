8fb8d4d488d9e31470dbcfb93ef395a5bdbdf7f0
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2026-06-19 12:29:53 -0300

    ADD: G02E02 TipoDePropiedad.java

---
+++ src/g02/e02/TipoDePropiedad.java
@@ -0,0 +1,13 @@
+package g02.e02;
+
+/**
+* Código generado por la app UXFtoJava by Charly Cimino
+* @see https://github.com/CharlyCimino/uxf-to-java
+*/
+public enum TipoDePropiedad {
+
+    DEPTO,
+    CASA,
+    PH;
+
+}
